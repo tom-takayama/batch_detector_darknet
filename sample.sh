25040f190956a15e62f9046b8c8e70374de7b156
tom-takayama <104437581+tom-takayama@users.noreply.github.com>
2022-04-28 14:55:54 +0900

    Add files via upload

---
+++ sample.sh
@@ -0,0 +1,3 @@
+Measure-Command{
+./darknet detector batch cfg/20192020_goushi.data cfg/yolov3_unka_test-960.cfg weights/yolov3_C18_B64-s16-W960-H960_4GPU_weight_best.weights io_folders ./sample_imgs/ ./output/ -out ./output/result.json -ext_output -dont_show -save_labels > ./output/result.txt
+}
