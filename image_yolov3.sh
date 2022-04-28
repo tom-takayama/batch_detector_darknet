f36ae43845df808a258db95a1a5d03ae83024a68
tom-takayama <104437581+tom-takayama@users.noreply.github.com>
2022-04-28 14:57:39 +0900

    Update image_yolov3.sh

--- image_yolov3.sh
+++ image_yolov3.sh
@@ -1,6 +1,5 @@
 
-
-./darknet detector test ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights data/dog.jpg -i 0 -thresh 0.25
+./darknet detector batch cfg/20192020_goushi.data cfg/yolov3_unka_test-960.cfg weights/yolov3_C18_B64-s16-W960-H960_4GPU_weight_best.weights io_folders ./sample_imgs/ ./output/ -out ./output/result.json -ext_output -dont_show -save_labels > ./output/result.txt
 
 
 
